faebf5d1673707d0d034de0effbfbec840a25063
marta-z <marta.zielinska@oneadvanced.com>
2018-01-18 10:33:20 +0000

    Package update

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
-    
+    Crawler = require("crawler");
+    cheerio = require('cheerio');
+
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
@@ -93,10 +95,36 @@ app.get('/pagecount', function (req, res) {
 });
 
 
-app.get('/date', function (req, res) {
-  res.send('Hello');
-});
+function myFunc(req, res, next){
 
+    var c = new Crawler({
+        maxConnections : 10
+    });
+
+    c.direct({
+        uri: 'http://www.bbc.co.uk/sport/football/scores-fixtures/' + req.params.matchdate.replace(':', ''),
+        skipEventRequest: false, 
+        callback: function (error, response) {
+            if(error){
+                return res.send('Error occured: ' + error)
+            }else{
+                var $ = response.$;
+                var Matchdate;
+
+                $('.sp-c-date-picker-timeline__item--selected').filter(function () {
+                    var data = $(this);
+                    Matchdate = data.children().first().attr('href');
+                    Matchdate = Matchdate.replace('/sport/football/scores-fixtures/', '');
+                  });
+
+                return res.send('Response here: ' + Matchdate);
+            }
+        }
+    });
+  
+}
+
+app.get('/date:matchdate', myFunc);
 
 // error handling
 app.use(function(err, req, res, next){
