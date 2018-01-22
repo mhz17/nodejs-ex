044a592fe3231d69808d74d2a4eb0dd8f7aeda7a
marta-z <marta.zielinska@oneadvanced.com>
2018-01-22 15:48:21 +0000

    Updated access headers

--- server.js
+++ server.js
@@ -291,8 +291,25 @@ app.get('/', (req, res) => {
 
 // error handling
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+  
+    // Pass to next layer of middleware
+    next();
+
+    console.error(err.stack);
+    res.status(500).send('Something bad happened!');
 });
 
 app.listen(port, ip);
