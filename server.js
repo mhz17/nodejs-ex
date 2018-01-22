16605a7ef5e443f4a32fc1bd64a3cdaf2fcbcd57
mhz17 <zielinska.marta@gmail.com>
2018-01-22 19:20:20 +0000

    access

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(morgan('combined'))
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://football-stats-56774.firebaseapp.com');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
